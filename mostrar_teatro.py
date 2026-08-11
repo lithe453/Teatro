5e4a3de9bd308443f9314f8772a22b9dbd823b9e
lithe453 <larissaarantesdonha@gmail.com>
2026-08-11 15:07:37 -0300

    Create mostrar_teatro.py

---
+++ mostrar_teatro.py
@@ -0,0 +1,4 @@
+def mostrar_teatro():
+
+    for x in range(len(teatro)):
+        print(f"fila {x} - {teatro[x][0]} {teatro[x][1]} {teatro[x][2]} {teatro[x][3]} {teatro[x][4]} {teatro[x][5]} {teatro[x][6]} {teatro[x][7]} {teatro[x][8]} {teatro[x][9]}")
